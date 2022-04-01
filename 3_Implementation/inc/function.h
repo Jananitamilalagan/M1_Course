ccc666c8cab8d91bf4e4a11f4d6f3e05ab0221ba
Jananitamilalagan <101245669+Jananitamilalagan@users.noreply.github.com>
2022-04-01 19:47:01 +0530

    Add files via upload

---
+++ 3_Implementation/inc/function.h
@@ -0,0 +1,26 @@
+#ifndef __FUNCTION_H__
+#define __FUNCTION_H__
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "math.h"
+#include "assert.h"
+#ifndef __FUNCTION_H__
+#define __FUNCTION_H__
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "math.h"
+#include "assert.h"
+#endif 
+int enterchoice(void);
+void details(void);
+int ListofMovies(void);
+int movieselect(void);
+int changepriceticket(int);
+void show1(int choice,char name[10],int id1,int price);
+void show2(int choice,char name[10],int id1,int price);
+void show3(int choice,char name[10],int id1,int price);
+void reserveticket(int *,int,int );
+void cancelticket(int *); 
+#endif 
